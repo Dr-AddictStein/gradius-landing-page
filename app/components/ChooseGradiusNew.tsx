7af2b9678ac133ac7e9c75a0814907d2f3a25000
harunbd80 <harun708280@gmail.com>
2025-03-04 22:03:08 +0600

    up

--- app/components/ChooseGradiusNew.tsx
+++ app/components/ChooseGradiusNew.tsx
@@ -23,7 +23,7 @@ const ChooseGradiusNew = () => {
           whileInView={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
           viewport={{ once: false, amount: 0.2 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
-          className="text-3xl md:text-7xl lg:text-[80px]  pb-5 md:mt-0 -mt-10 font-medium leading-tight bg-gradient-to-b from-[#6E27D5] to-[#39146F] text-transparent bg-clip-text drop-shadow-[0px_0px_15px_#A436F1]"
+          className="text-3xl md:text-7xl lg:text-[80px]  pb-3 md:mt-0 -mt-10 font-medium leading-tight bg-gradient-to-b from-[#6E27D5] to-[#39146F] text-transparent bg-clip-text drop-shadow-[0px_0px_15px_#A436F1]"
         >
           Cognitive Research
         </motion.h1>
